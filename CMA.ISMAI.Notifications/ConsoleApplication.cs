97575bc6be3d7acacbf9da76e31e64384520f1d0
Carlos Campos <carlosmiguelazevedocampos1996@hotmail.com>
2020-04-05 01:06:23 +0100

    + notifications now can be configurated using appsettings.json

---
+++ CMA.ISMAI.Notifications/ConsoleApplication.cs
@@ -0,0 +1,87 @@
+﻿using CMA.ISMAI.Core.Notifications;
+using CMA.ISMAI.Logging.Interface;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace CMA.ISMAI.Notifications
+{
+    public class ConsoleApplication : BaseConfiguration
+    {
+        private readonly ILog _log;
+
+        public ConsoleApplication(ILog log)
+        {
+            this._log = log;
+        }
+        internal void Run()
+        {
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = ReturnSettingsValue("RabbitMq", "Uri"),
+                    Port = Convert.ToInt32(ReturnSettingsValue("RabbitMq", "Port")),
+                    UserName = ReturnSettingsValue("RabbitMq", "Username"),
+                    Password = ReturnSettingsValue("RabbitMq", "Password")
+                };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: ReturnSettingsValue("RabbitMq", "Queue"),
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+                    channel.BasicConsume(queue: ReturnSettingsValue("RabbitMq", "Queue"),
+                         autoAck: true,
+                         consumer: consumer);
+
+                    Console.WriteLine("Processing Notifications..");
+                    Console.ReadKey();
+                }
+            }
+            catch(Exception ex)
+            {
+                _log.Fatal(ex.ToString());
+            }
+        }
+
+        private void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            try
+            {
+                string email = ReturnSettingsValue("Notification", "Email");
+                MessageBody notification = JsonConvert.DeserializeObject<MessageBody>
+                    (Encoding.UTF8.GetString(e.Body));
+                Console.WriteLine($"New notification recived! Message Body {notification.Message}");
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress(email);
+                message.To.Add(new MailAddress(notification.To));
+                message.Subject = string.Format("ISMAI - {0}", notification.Message.GetType());
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = notification.Message;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(email,
+                    ReturnSettingsValue("Notification", "Password"));
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+                _log.Fatal(ex.ToString());
+            }
+        }
+    }
+}
--- CMA.ISMAI.Notifications/Program.cs
+++ CMA.ISMAI.Notifications/Program.cs
@@ -1,71 +1,43 @@
-﻿using CMA.ISMAI.Core.Notifications;
-using Newtonsoft.Json;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+﻿using CMA.ISMAI.Logging.Interface;
+using CMA.ISMAI.Logging.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using System;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
 
 namespace CMA.ISMAI.Notifications
 {
-    class Program
+    class Program : BaseConfiguration
     {
+
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory()
-            {
-                HostName = "localhost",
-                Port = 5672,
-                UserName = "admin",
-                Password = "admin"
-            };
+            var services = ConfigureServices();
 
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "NotificationsQueue",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+            var serviceProvider = services.BuildServiceProvider();
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            loggerFactory.AddSerilog();
 
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += Consumer_Received;
-                channel.BasicConsume(queue: "NotificationsQueue",
-                     autoAck: true,
-                     consumer: consumer);
-
-                Console.WriteLine("Processing Notifications..");
-                Console.ReadKey();
-            }
+            serviceProvider.GetRequiredService<ConsoleApplication>().Run();
+            Console.ReadKey();
         }
 
-        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        private static IServiceCollection ConfigureServices()
         {
-            try
-            {
-                MessageBody notification = JsonConvert.DeserializeObject<MessageBody>(Encoding.UTF8.GetString(e.Body));
-                Console.WriteLine($"New notification recived! Message Body {notification.Message}");
-                MailMessage message = new MailMessage();
-                SmtpClient smtp = new SmtpClient();
-                message.From = new MailAddress("trelloismai@gmail.com");
-                message.To.Add(new MailAddress(notification.To));
-                message.Subject = "ISMAI";
-                message.IsBodyHtml = true; //to make message body as html  
-                message.Body = notification.Message;
-                smtp.Port = 587;
-                smtp.Host = "smtp.gmail.com"; //for gmail host  
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials =false;
-                smtp.Credentials = new NetworkCredential("trelloismai@gmail.com", "latiascampos");
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtp.Send(message);
-            }
-            catch(Exception ex)
-            {
+            IServiceCollection services = new ServiceCollection();
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(ReturnSettingsValue("ElasticConfiguration", "Uri")))
+               {
+                   AutoRegisterTemplate = true,
+               })
+            .CreateLogger();
 
-            }
+            services.AddLogging();
+            services.AddTransient<ConsoleApplication>();
+            services.AddScoped<ILog, LoggingService>();
+            return services;
         }
     }
 }
